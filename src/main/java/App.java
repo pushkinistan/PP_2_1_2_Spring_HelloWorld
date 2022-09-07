b85266562f47f1bb7a3f458ac55f779466b0daae
Бойченко Вадим Валерьевич <vadim.boychenko@gmail.com>
2022-09-07 18:56:36 +0300

    fin

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld hw1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(hw1.getMessage());
+        HelloWorld hw2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(hw2.getMessage());
+        System.out.println(hw1 == hw2);
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1 == cat2);
+
     }
 }
